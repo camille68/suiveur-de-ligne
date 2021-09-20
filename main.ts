a33cea7e7b6e3d39cca99ba5dd77aba73c5c0269
camille68 <camille68@gmail.com>
2021-09-20 22:22:37 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,4 @@
-let detecteur_droit = 0
-let detecteur_gauche = 0
-pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+kitronik_servo_lite.forward()
 basic.forever(function () {
-    detecteur_gauche = pins.digitalReadPin(DigitalPin.P16)
-    detecteur_droit = pins.digitalReadPin(DigitalPin.P15)
-    if (detecteur_gauche == 1 && detecteur_droit == 1) {
-        basic.showNumber(11)
-    } else if (detecteur_gauche == 0 && detecteur_droit == 1) {
-        basic.showNumber(1)
-    } else if (detecteur_gauche == 1 && detecteur_droit == 0) {
-        basic.showNumber(2)
-    } else if (detecteur_gauche == 0 && detecteur_droit == 0) {
-        basic.showNumber(0)
-    }
+	
 })
