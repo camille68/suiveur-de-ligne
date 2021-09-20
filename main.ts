299b267049be2cb7a98f8a62c77e318db2e3c8d9
camille68 <camille68@gmail.com>
2021-09-20 22:23:00 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
-kitronik_servo_lite.forward()
+let detecteur_droit = 0
+let detecteur_gauche = 0
+pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
 basic.forever(function () {
-	
+    detecteur_gauche = pins.digitalReadPin(DigitalPin.P16)
+    detecteur_droit = pins.digitalReadPin(DigitalPin.P15)
+    if (detecteur_gauche == 0 && detecteur_droit == 0) {
+        kitronik_servo_lite.forward()
+    } else if (detecteur_gauche == 1 && detecteur_droit == 0) {
+        kitronik_servo_lite.right()
+    } else if (detecteur_gauche == 0 && detecteur_droit == 1) {
+        kitronik_servo_lite.left()
+    } else if (detecteur_gauche == 1 && detecteur_droit == 1) {
+        kitronik_servo_lite.stop()
+    }
 })
